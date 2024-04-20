b9aa93aec20e2997735006acf18af01bd8389305
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-04-20 17:35:58 -0400

    1

--- home.php
+++ home.php
@@ -105,11 +105,12 @@
     </div>
 
     <div class="videoContainer">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Y2BV1zJjmtA?si=Rlz9L1SgpnRZnUAJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Y2BV1zJjmtA" title="MUSIC BUFF RADIO LIVE ON Q96TV" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   
+     
     <div class="videoContainer">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/DLr3nECDqJ0?si=r_hvJF5PSGeiwJ15" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/tPtpHZZHmr4" title="Trevor Forde LIVE on Q96TV with  Renaizancerz" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 
     <button class="scrollButton darken" onclick="scrollDown()"><img src="images/arrow.png"></button>
